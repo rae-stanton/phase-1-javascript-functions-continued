5b6d1c96a7ae9f7280caada330617f594dee4cc9
Rae Stanton <raestanton93@gmail.com>
2023-07-31 13:17:32 -0700

    more functions!
    functionally more functions

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return "This Saturday, I want to " + activity + "!";
+}
+
+function mondayWork(ifEmpty = "go to the office") {
+  return "This Monday, I will " + (ifEmpty) + '.';
+}
+
+function wrapAdjective(highlight = "*") {
+  return function (adjective) {
+    return "You are " + highlight + adjective + highlight + "!";
+  };
+}
